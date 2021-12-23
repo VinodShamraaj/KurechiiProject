47ad82e5f2923427acdbd5d1859ba1a9ac1e957a
VinodShamraaj <vinodshamraaj@gmail.com>
2021-12-23 12:40:41 +0800

    Added Nodes and Connection Classes
    Nodes and Connections make up the structure of the problem

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection
+{
+    
+    public Node connectedNode;
+    public int cost;
+
+    public Connection(Node newnode, int resource)
+    {
+        this.connectedNode = newnode;
+        this.cost = resource;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Node
+{
+    public string nodeName;
+    public int nuts;
+    public Connection[] connections;
+}
+
